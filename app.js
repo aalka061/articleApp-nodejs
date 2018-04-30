c4857ed72cfba54384e846bf14b889ab9fe5ee24
aalka061 <a_algg@hotmail.com>
2018-04-30 01:40:53 -0400

    add a tages to article titles

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(session({
 }));
 
 app.use(expressValidator())
-app.use('/assets', express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
 //express messages middleware
